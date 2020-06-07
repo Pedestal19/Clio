dddb237f88b3079e411d68ba68466b9d0abc5221
Pedestal19 <gabeoji01@gmail.com>
2020-06-07 16:45:04 +0100

    using viewmodel for activity state

---
+++ app/src/main/java/com/hosannagabe/notekeeper/NoteActivityViewModel.java
@@ -0,0 +1,9 @@
+package com.hosannagabe.notekeeper;
+
+import androidx.lifecycle.ViewModel;
+
+public class NoteActivityViewModel extends ViewModel {
+    public String mOriginalNoteCourseId;
+    public String mOriginalNoteTitle;
+    public String mOriginalNoteText;
+}
